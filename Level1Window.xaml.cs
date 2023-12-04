64379473b66def85241a6b5c5afc24d1770d111e
setoju48@gmail.com <setoju48@gmail.com>
2023-12-04 10:39:23 +0200

    Implemented the collision logic on the first level

--- Level1Window.xaml.cs
+++ Level1Window.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
@@ -20,7 +22,8 @@ namespace CupFilling
     /// </summary>
     public partial class Level1Window : Window
     {
-        private List<Wall> walls = new List<Wall>();
+        //private List<Wall> walls = new List<Wall>();
+        private bool gameEnded = false;
         private static PointCollection cupPoints = new PointCollection
             {
                 new Point(390, 700),
@@ -35,9 +38,10 @@ namespace CupFilling
         {
             InitializeComponent();
                                               
-            FirstLevelCanvas.Children.Add(firstLevelCup.ShowCup());
-            AddWalls();
+            FirstLevelCanvas.Children.Add(firstLevelCup.ShowCup());            
+            //AddWalls();
             FirstLevelCanvas.MouseLeftButtonDown += OnCanvasClick;
+
         }
         private void OnCanvasClick(object sender, MouseButtonEventArgs e)
         {
@@ -54,7 +58,7 @@ namespace CupFilling
             {
                 MessageBox.Show("You failed");
                 this.Close();
-            }            
+            }
 
             Ellipse ball = new Ellipse
             {
@@ -82,16 +86,42 @@ namespace CupFilling
             timer.Tick += (s, e) =>
             {
                 double newY = Canvas.GetTop(ball) + 5; // Adjusting the ball falling speed
-                //!!!                                                        
-                // Maybe when the ball is outside of the window we should stop its timer to optimize the game
-                //!!!^
-                // Checking if the ball is colliding with any wall
-                foreach (var wall in walls)
-                {                                     
-                    if (wall.IsColliding(ball))
+                double newX = Canvas.GetLeft(ball);    //!!!                                                        
+                                                       // Maybe when the ball is outside of the window we should stop its timer to optimize the game
+                                                       //!!!^
+                // Checking if the ball is colliding with any wall                                                      
+                Rect ballBounds = new Rect(Canvas.GetLeft(ball), Canvas.GetTop(ball), ball.Width, ball.Height);
+                foreach (var x in FirstLevelCanvas.Children.OfType<Rectangle>())
+                {
+                    if ((string)x.Tag == "wall")
                     {
-                        timer.Stop();
-                        return;
+                        Point ballCenter = new Point(ballBounds.X + ballBounds.Width / 2, ballBounds.Y + ballBounds.Height / 2);
+
+                        // Get the rotation angle of the wall
+                        var rotateTransform = x.RenderTransform as RotateTransform;
+                        double angle = rotateTransform != null ? -rotateTransform.Angle : 0;
+
+                        // Calculate the rotated position of the ball center
+                        double radians = Math.PI * angle / 180.0;
+                        double rotatedX = Math.Cos(radians) * (ballCenter.X - Canvas.GetLeft(x)) - Math.Sin(radians) * (ballCenter.Y - Canvas.GetTop(x)) + Canvas.GetLeft(x);
+                        double rotatedY = Math.Sin(radians) * (ballCenter.X - Canvas.GetLeft(x)) + Math.Cos(radians) * (ballCenter.Y - Canvas.GetTop(x)) + Canvas.GetTop(x);
+
+                        // Create a rotated bounding box for the ball
+                        Rect rotatedBallBounds = new Rect(rotatedX - ballBounds.Width / 2, rotatedY - ballBounds.Height / 2, ballBounds.Width, ballBounds.Height);
+
+                        // Check for intersection between the rotated ball and the rotated wall
+                        if (rotatedBallBounds.IntersectsWith(new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height)))
+                        {
+                            if (rotateTransform.Angle == 45)
+                            {
+                                newX += 5;
+                            }
+                            else if(rotateTransform.Angle == 135)
+                            {
+                                newX -= 5;
+                            }                            
+                            // Assuming you want to stop the timer immediately upon collision
+                        }
                     }
                 }
                 // Checking if the ball has reached the cup 
@@ -99,21 +129,38 @@ namespace CupFilling
                 {
                     if (!firstLevelCup.FillIfNotFull())
                     {
-                        this.Close();
-                    }                        
+                        timer.Stop();
+                        if (!gameEnded)
+                        {
+                            gameEnded = true;
+
+                            MessageBoxResult result = MessageBox.Show("Congratulations, you won. Do you want to play the next level?", "Confirmation", MessageBoxButton.YesNo);
+
+                            if (result == MessageBoxResult.Yes)
+                            {
+                                StartNextLevel();
+                            }
+                            else
+                            {
+                                this.Close();
+                            }
+                        }                        
+                    }
                     timer.Stop();
                 }
                 else
                 {
                     // Moving the ball down                        
                     Canvas.SetTop(ball, newY);
-                }                               
+                    Canvas.SetLeft(ball, newX);
+                }
             };
 
             timer.Start();
 
             // ???????
             previousBallTop += ball.Height;
+        }
 
         public class Level1 : Level
         {
@@ -131,33 +178,38 @@ namespace CupFilling
         }
         private void NextLevelButton_Click(object sender, RoutedEventArgs e)
         {
+            StartNextLevel();
+        }
+        private void StartNextLevel()
+        {
             Level2Window.Level2 startNext = new Level2Window.Level2();
             this.Close();
             startNext.StartLevel();
         }
         // Method for adding walls to the canvas
-        private void AddWalls()
-        {            
-            Rectangle wallRectangle = new Rectangle
-            {
-                Width = 100,
-                Height = 5,
-                Fill = Brushes.Gray
-            };
 
-            Canvas.SetLeft(wallRectangle, 200);
-            Canvas.SetTop(wallRectangle, 600);
+        //private void AddWalls()
+        //{
+        //    Rectangle wallRectangle = new Rectangle
+        //    {
+        //        Width = 100,
+        //        Height = 5,
+        //        Fill = Brushes.Gray
+        //    };
 
-            RotateTransform rotateTransform = new RotateTransform(45); // Wall angle
-            wallRectangle.RenderTransform = rotateTransform;
+        //    Canvas.SetLeft(wallRectangle, 200);
+        //    Canvas.SetTop(wallRectangle, 600);
 
-            Wall diagonalWall = new Wall(wallRectangle, 45);
-            walls.Add(diagonalWall);
-            
-            foreach (Wall wall in walls)
-            {
-                FirstLevelCanvas.Children.Add(wall.GetPosition());
-            }            
-        }        
+        //    RotateTransform rotateTransform = new RotateTransform(45); // Wall angle
+        //    wallRectangle.RenderTransform = rotateTransform;                        
+
+        //    Wall diagonalWall = new Wall(wallRectangle, 45);
+        //    walls.Add(diagonalWall);
+
+        //    foreach (Wall wall in walls)
+        //    {
+        //        FirstLevelCanvas.Children.Add(wall.GetPosition());
+        //    }
+        //}                
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -101,9 +101,7 @@ namespace CupFilling
                 return true;
             }                            
             else
-            {
-                // <ERROR> Fix the bug when you spam left click and we have too many message boxes
-                MessageBox.Show("Congratulations, you won!");                
+            {                                              
                 return false;
             }
         }
@@ -112,75 +110,16 @@ namespace CupFilling
     public class Wall
     {
         private Rectangle _position;
-        private double _angle;
+        private double _angle;        
 
         public Wall(Rectangle position, double angle)
         {
             _position = position;
-            _angle = angle;
+            _angle = angle;           
         }
         public bool IsColliding(Ellipse ball)
         {
-            double angleInRadians = _angle * Math.PI / 180.0;
-
-            Rect rectBounds = _position.TransformToAncestor((Visual)_position.Parent)
-                .TransformBounds(new Rect(_position.RenderSize));
-
-            Rect ellipseBounds = ball.TransformToAncestor((Visual)ball.Parent)
-                .TransformBounds(new Rect(ball.RenderSize));
-
-            // Check for intersection of bounding rectangles
-            if (!rectBounds.IntersectsWith(ellipseBounds))
-            {
-                return false;
-            }
-
-            // Check for more precise collision using geometric calculations
-            Point rectCenter = new Point(rectBounds.X + rectBounds.Width / 2, rectBounds.Y + rectBounds.Height / 2);
-            Point ellipseCenter = new Point(ellipseBounds.X + ellipseBounds.Width / 2, ellipseBounds.Y + ellipseBounds.Height / 2);
-
-            double distanceX = Math.Abs(rectCenter.X - ellipseCenter.X);
-            double distanceY = Math.Abs(rectCenter.Y - ellipseCenter.Y);
-
-            double halfRectWidth = rectBounds.Width / 2;
-            double halfRectHeight = rectBounds.Height / 2;
-
-            double radiusX = ellipseBounds.Width / 2;
-            double radiusY = ellipseBounds.Height / 2;
-
-            // Rotate the point of the rectangle center
-            double rotatedX = Math.Cos(angleInRadians) * (rectCenter.X - rectBounds.X) -
-                              Math.Sin(angleInRadians) * (rectCenter.Y - rectBounds.Y) +
-                              rectBounds.X;
-
-            double rotatedY = Math.Sin(angleInRadians) * (rectCenter.X - rectBounds.X) +
-                              Math.Cos(angleInRadians) * (rectCenter.Y - rectBounds.Y) +
-                              rectBounds.Y;
-
-            // Check if the point is on the other side of the rotated rectangle
-            if (rotatedX < rectBounds.X || rotatedX > rectBounds.X + rectBounds.Width ||
-                rotatedY < rectBounds.Y || rotatedY > rectBounds.Y + rectBounds.Height)
-            {
-                return false;
-            }
-
-            double rotatedDistanceX = Math.Abs(rotatedX - ellipseCenter.X);
-            double rotatedDistanceY = Math.Abs(rotatedY - ellipseCenter.Y);
-
-            if (rotatedDistanceX > (halfRectWidth + radiusX) || rotatedDistanceY > (halfRectHeight + radiusY))
-            {
-                return false;
-            }
-
-            if (rotatedDistanceX <= halfRectWidth || rotatedDistanceY <= halfRectHeight)
-            {
-                return true;
-            }
-
-            double cornerDistanceSquared = Math.Pow(rotatedDistanceX - halfRectWidth, 2) +
-                                           Math.Pow(rotatedDistanceY - halfRectHeight, 2);
-
-            return cornerDistanceSquared <= Math.Pow(radiusX, 2);
+            return true;
         }
         public Rectangle GetPosition()
         {
